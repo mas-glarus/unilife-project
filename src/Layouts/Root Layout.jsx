a499dfcf9a7b05584c5ae6d6c0de88d997a07433
Pablo <you@example.com>
2023-03-26 13:20:24 +0200

    1st commit, added main layout routes

---
+++ src/Layouts/Root Layout.jsx
@@ -0,0 +1,24 @@
+import 
+{ createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link
+ } from 'react-router-dom';
+
+
+
+ 
+const RootLayout = () => {
+    return (
+
+    <div className="rootlayout">
+        <h1>UniLIFE</h1>
+        <div>
+            <link>bla </link>
+            <link>bla</link>
+        </div>
+    </div>
+    )
+}
+
+export default RootLayout;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+
+
+const HomePage = () => {
+
+    return(
+        <div>
+            <h1>HOMEPAGE</h1>
+
+        </div>
+    )
+}
+
+export default HomePage;
+
