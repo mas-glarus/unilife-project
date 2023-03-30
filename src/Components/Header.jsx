164a7d347f767dc464cfd62cca3b85014396e1b1
Nikolay Kolin <49337908+NikolayKolin@users.noreply.github.com>
2023-03-30 13:38:04 +0200

    Header and Hero Components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import '../styles/Header.css';
+import { Link } from 'react-router-dom';
+import { FaRegEnvelope } from 'react-icons/fa';
+import { AiOutlineHeart } from 'react-icons/Ai';
+import logo from '../assets/UniLife.png';
+import vector from '../assets/Vector.svg';
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="header" >
+      <div className="logo">
+        <Link to="/">
+          <img className="vector" src={vector} alt="Logo" />
+          <img className="logo-text"src={logo} alt="Logo" />
+        </Link>
+      </div>
+      <div className="actions" >
+        <button className="shortlist">
+          <AiOutlineHeart /> <span>Shortlist</span>
+        </button>
+        <Link to="/contact" className="contact-us">
+          <FaRegEnvelope/> <span>Contact Us</span>
+        </Link>
+      </div>
+      <input type="checkbox" id="hamburger" checked={isOpen} onChange={handleToggle} className="hamburger-checkbox" />
+      <label htmlFor="hamburger" className="hamburger" >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+      <div className={`mobile-menu ${isOpen ? "open" : ""}`}>
+        <div className="menu-dropdown">
+          <button className="shortlist">
+            <AiOutlineHeart /> <span>Shortlist</span>
+          </button>
+          <Link to="/contact" className="contact-us">
+            <FaRegEnvelope /> <span>Contact Us</span>
+          </Link>
+        </div>
+      </div>
+      
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Components/HeroHome.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../styles/HeroHome.css";
+
+const HeroHome = () => {
+  return (
+    <div className='hero-section'>
+      <img src="../src/assets/hero-img.jpg" alt="hero" />
+      <div className='title'>
+        <h1>Find student homes with bills included</h1>
+        <p>A simple and faster way to research for student accomodation</p>
+      </div>
+    </div>
+  );
+
+}
+export default HeroHome;
+
