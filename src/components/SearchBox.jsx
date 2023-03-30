21fa08fce60486ef729003a981c56cb7576b8317
marc aschwanden <marc.aschwanden88@gmail.com>
2023-03-30 21:32:02 +0200

    create api connection for cities

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,44 +1,53 @@
 import Select from "react-select";
+import React, { useState } from "react";
+import getCities from "../api/getCities";
+import AsyncSelect from "react-select/async";
 
 import "../assets/searchbox.css";
 
-const cities = [
-	{ label: "Leeds", value: "Leeds" },
-	{ label: "Newcastle", value: "Newcastle" },
-	{ label: "Sheffield", value: "Sheffield" },
-	{ label: "Southampton", value: "Southampton" },
-	{ label: "Leicester", value: "Leicester" },
-	{ label: "Brighton", value: "Brighton" },
-];
-
-const bedrooms = [
-	{ label: "1", value: "one" },
-	{ label: "2", value: "two" },
-	{ label: "3", value: "three" },
-	{ label: "4", value: "four" },
-	{ label: "5", value: "five" },
-	{ label: "6", value: "six" },
-];
-
 const SearchBox = () => {
+	const [inputValue, setValue] = useState("");
+	const [selectedValue, setSelectedValue] = useState(null);
+
+	//handle input change event
+	const handleInputChange = (value) => {
+		setValue(value);
+	};
+
+	//handle selection
+	const handleChange = (value) => {
+		setSelectedValue(value);
+	};
+
+	const fetchData = () => {
+		return getCities.get("/cities").then((result) => {
+			const res = result.data.response;
+			return res;
+		});
+	};
+
 	return (
 		<div>
 			<h1>Searchbox</h1>
+			<div className="test-log">
+				Selected Value: {JSON.stringify(selectedValue || {}, null, 2)}
+			</div>
 			<div className="search-container">
 				<div className="select-container">
-					<Select
-						className="select cities"
-						options={cities}
-						placeholder="Search by city"
-					/>
-					<Select
-						className="select bedrooms"
-						options={bedrooms}
-						placeholder="Any bedroom"
+					<AsyncSelect
+						cacheOptions
+						defaultOptions
+						value={selectedValue}
+						getOptionLabel={(e) => e.name}
+						getOptionValue={(e) => e._id}
+						loadOptions={fetchData}
+						onInputChange={handleInputChange}
+						onChange={handleChange}
+						className="select"
 					/>
 				</div>
-				<button>Find Homes</button>
 			</div>
+			<img src={selectedValue.image_url} />
 		</div>
 	);
 };
