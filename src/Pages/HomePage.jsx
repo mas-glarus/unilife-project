85a709c623aab2c08fc4441775ca9766f53983b1
Nikolay Kolin <49337908+NikolayKolin@users.noreply.github.com>
2023-03-30 13:37:50 +0200

    Header and Hero Components

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Components/Header';
+import HeroHome from '../Components/HeroHome.jsx';
+
+const HomePage = () => {
+	return (
+		<>
+		<Header/>
+     	<HeroHome/>
+      	
+		</>
+	);
+};
+
+export default HomePage;
